6e18c74ce1cdf49a4e4bdfc4fd20ba93c33c62a2
bhernandez <bhernandez@iuvity.net>
2025-03-21 12:45:14 -0500

    Primer commit

---
+++ src/main/java/com/co/hackathon/itm_hackathon_web/ItmHackathonWebApplication.java
@@ -0,0 +1,13 @@
+package com.co.hackathon.itm_hackathon_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmHackathonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItmHackathonWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/hackathon/itm_hackathon_web/ItmHackathonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.hackathon.itm_hackathon_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmHackathonWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
