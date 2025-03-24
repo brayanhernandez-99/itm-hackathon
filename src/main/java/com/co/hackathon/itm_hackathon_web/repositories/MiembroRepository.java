14f7b564d669b15afce71734eed56a21dfdd3409
bhernandez <bhernandez@iuvity.net>
2025-03-24 00:09:21 -0500

    Correccion interfaces

--- src/main/java/com/co/hackathon/itm_hackathon_web/repositories/MiembroRepository.java
+++ src/main/java/com/co/hackathon/itm_hackathon_web/repositories/MiembroRepository.java
@@ -2,6 +2,8 @@ package com.co.hackathon.itm_hackathon_web.repositories;
 
 import com.co.hackathon.itm_hackathon_web.models.Miembro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MiembroRepository extends JpaRepository<Miembro, Integer> {
 }
